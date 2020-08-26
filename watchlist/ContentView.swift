53e371451703929b2a5fb4cb60a9fe41ae1295aa
Reed <reedmg15@gmail.com>
2020-08-25 19:25:13 -0700

    Initial Commit

---
+++ watchlist/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  watchlist
+//
+//  Created by Reed Metzler-Gilbertz on 8/25/20.
+//  Copyright © 2020 Reed Metzler-Gilbertz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
